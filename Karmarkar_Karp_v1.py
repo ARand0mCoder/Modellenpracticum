b4a013e3e7daa732a809b3c204972fdee9318087
WillemvOosterhout <48512958+WillemvOosterhout@users.noreply.github.com>
2019-03-16 17:28:55 +0100

    Add files via upload

---
+++ Karmarkar_Karp_v1.py
@@ -0,0 +1,46 @@
+##Karmarkar-Karp with fixed norm (i.e. norm is not updated! K-K with updating norm will (hopefully) follow soon)
+def Norm(array): #Choose your own norm. Average of the highest n is also possible
+    # Make sure that Norm greater or equal to 0
+    return np.amax(np.abs(array))
+    
+def Karmarkar_Karp(power): #only for m=2
+    data0 = np.zeros(len(power))
+    for i in range(len(power)):
+        data0[i] = Norm(power[i])
+    data1 = list(range(0,len(data0)))
+    data = [data0,data1]    #data0 consists of the norms of the actual data, data1 is the index corresponding to the norm
+    
+    connectlines = [[] for i in range(len(data[0])-1)]  #If one considers the norms as points, the first step of K-K is to find the lines which connect these points
+    
+    for i in range(len(data[0])-1):
+        data[0],data[1] = zip(*sorted(zip(data[0],data[1])))
+        data = [np.array(data[0]),np.array(data[1])]
+        data[0][-1] = data[0][-1] - data[0][-2]
+        data[0][-2] = 0
+        connectlines[i].append(data[1][-1])
+        connectlines[i].append(data[1][-2])
+        data = np.delete(data, -2, 1)
+    
+    station1 = []
+    station2 = []
+    
+    station1.append(connectlines[0][0]) #The second step of K-K is to colour the obtained graph with two colours, s.t. points which are connected by a line have opposite colors. This will give the desired two groups.
+    for i in range(len(connectlines)-1):    #Probably a faster way to do this, but this works.
+        for elem in station1:
+            for elem2 in connectlines:
+                if elem in elem2:
+                    elem2.remove(elem)            
+                    station2 = np.append(station2,elem2)
+                    connectlines.remove(elem2)
+                    station2 = np.hstack(station2)
+        for elem in station2:            
+            for elem2 in connectlines:
+                if elem in elem2:
+                    elem2.remove(elem)            
+                    station1 = np.append(station1,elem2)
+                    connectlines.remove(elem2)
+                    station1 = np.hstack(station1)
+                    
+    print(station1)
+    print(station2)
+    
