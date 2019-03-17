932114b14f0391cf1bf76335fdb89f3547e9ba1e
WillemvOosterhout <48512958+WillemvOosterhout@users.noreply.github.com>
2019-03-17 14:01:40 +0100

    Update Karmarkar_Karp_v1.py

--- Karmarkar_Karp_v1.py
+++ Karmarkar_Karp_v1.py
@@ -1,4 +1,4 @@
-##Karmarkar-Karp   #Still seems to be a lot worse than greedy algorithm
+##Karmarkar-Karp   #Still seems to be a bit worse than greedy algorithm
 def Norm(array): #Choose your own norm. Average of the highest n is also possible
     # Make sure that Norm greater or equal to 0
     return np.amax(np.abs(array))
@@ -9,13 +9,17 @@ def Karmarkar_Karp(power): #only for m=2
         data0[i] = Norm(power[i])
     data1 = list(range(0,len(data0)))
     data = [data0,data1]    #data0 consists of the norms of the actual data, data1 is the index corresponding to the norm
-    
+    powercopy = np.copy(power)
+  
     connectlines = [[] for i in range(len(data[0])-1)]  #If one considers the norms as points, the first step of K-K is to find the lines which connect these points
     
     for i in range(len(data[0])-1):
         data[0],data[1] = zip(*sorted(zip(data[0],data[1])))
         data = [np.array(data[0]),np.array(data[1])]
-        data[0][-1] = Norm(power[1][-1] - data[1][-2]) #update norm
+        powercopy[int(data[1][-1])] = powercopy[int(data[1][-1])] - powercopy[int(data[1][-2])] #update norm
+        powercopy[int(data[1][-1])] = 0
+        print(powercopy)
+        data[0][-1] = Norm(powercopy[int(data[1][-1])]) 
         #data[0][-1] = data[0][-1] - data[0][-2]    #don't update norm
         data[0][-2] = 0
         connectlines[i].append(data[1][-1])
@@ -52,4 +56,3 @@ print(max(powerstation1))
 for i in station2:
     powerstation2 += power[int(i)]
 print(max(powerstation2))
-    
