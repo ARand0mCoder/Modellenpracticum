ea61e6d5ed395c37b9287984297c66709922bf99
ARand0mCoder <d.leijnse@gmail.com>
2019-03-19 12:02:20 +0100

    Update Karmarkar_Karp_v1.py

--- Karmarkar_Karp_v1.py
+++ Karmarkar_Karp_v1.py
@@ -1,24 +1,23 @@
+import numpy as np
+
 ##Karmarkar-Karp   #Still seems to be a bit worse than greedy algorithm
-def Norm(array): #Choose your own norm. Average of the highest n is also possible
-    # Make sure that Norm greater or equal to 0
-    return np.amax(np.abs(array))
     
-def Karmarkar_Karp(power): #only for m=2
+def Karmarkar_Karp(power, Norm): #only for m=2
     data0 = np.zeros(len(power))
     for i in range(len(power)):
         data0[i] = Norm(power[i])
-    data1 = list(range(0,len(data0)))
+    data1 = list(range(0, len(data0)))
     data = [data0,data1]    #data0 consists of the norms of the actual data, data1 is the index corresponding to the norm
     powercopy = np.copy(power)
   
     connectlines = [[] for i in range(len(data[0])-1)]  #If one considers the norms as points, the first step of K-K is to find the lines which connect these points
     
     for i in range(len(data[0])-1):
-        data[0],data[1] = zip(*sorted(zip(data[0],data[1])))
+        data[0], data[1] = zip(*sorted(zip(data[0],data[1])))
         data = [np.array(data[0]),np.array(data[1])]
         powercopy[int(data[1][-1])] = powercopy[int(data[1][-1])] - powercopy[int(data[1][-2])] #update norm
         powercopy[int(data[1][-1])] = 0
-        print(powercopy)
+        #print(powercopy)
         data[0][-1] = Norm(powercopy[int(data[1][-1])]) 
         #data[0][-1] = data[0][-1] - data[0][-2]    #don't update norm
         data[0][-2] = 0
@@ -47,12 +46,3 @@ def Karmarkar_Karp(power): #only for m=2
                     station1 = np.hstack(station1)
                     
     return(station1,station2)
-    
-station1, station2 = Karmarkar_Karp(power)  #find the actual max
-powerstation1, powerstation2 = np.zeros(len(power[0])), np.zeros(len(power[0]))
-for i in station1:
-    powerstation1 += power[int(i)]
-print(max(powerstation1))
-for i in station2:
-    powerstation2 += power[int(i)]
-print(max(powerstation2))
