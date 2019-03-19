aafd52c307cac1640758dc5122ac3590813e6b9a
ARand0mCoder <d.leijnse@gmail.com>
2019-03-19 12:04:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ import sys
 file_dir = r'C:\Users\dion\Documents\Studie\Modellenpracticum'
 sys.path.append(file_dir)
 
-import Dataverwerking
-import GreedyAlgorithm
+import Dataverwerking as Dv
+import GreedyAlgorithm as GA
+import Karmarkar_karp_v1 as kkk
+import MonteCarloTwist as MCT
 
 #%%
+def Norm(array): #Choose your own norm. Average of the highest n is also possible
+    # Make sure that Norm greater or equal to 0
+    return np.amax(np.abs(array))
+
+
+#%%
+
 
 path1 = r'C:\Users\dion\Documents\Studie\Modellenpracticum\Data\Uilenburg_1_2018_clean.csv'
 path2 = r'C:\Users\dion\Documents\Studie\Modellenpracticum\Data\Uilenburg_2_2018_clean.csv'
@@ -17,14 +26,14 @@ path2 = r'C:\Users\dion\Documents\Studie\Modellenpracticum\Data\Uilenburg_2_2018
 rows_to_trim1 = [0, 1, 2, 3, 4, 5, 8, 9]
 rows_to_trim2 = [0, 2, 3, 4, 7]
 
-power1 = Dataverwerking.read_data(path1)
-power2 = Dataverwerking.read_data(path2)
+power1, time1 = Dv.read_data(path1)
+power2, time2 = Dv.read_data(path2)
 
 power1 = np.transpose(np.array(power1))
 power2 = np.transpose(np.array(power2))
 
-power1 = Dataverwerking.auto_trim(power1, rows_to_trim1)
-power2 = Dataverwerking.auto_trim(power2, rows_to_trim2)
+power1 = Dv.auto_trim(power1, rows_to_trim1)
+power2 = Dv.auto_trim(power2, rows_to_trim2)
 #%%
 power = []
 for row in power1:
@@ -32,7 +41,8 @@ for row in power1:
 for row in power2:
     power.append(row[0:34944])
 
-solution = GreedyAlgorithm.FirstAlgorithm(power, 2)
+#%%
+solution1 = GA.FirstAlgorithm(power, 2, Norm)
 
 #%%
 
@@ -43,3 +53,12 @@ def plot_solution(power, sol, m):
             tot += power[j]
         plt.plot(tot)
         plt.show()
+#%%
+
+def sol_norms(power, sol, Norm):
+    for group in sol:
+        tot = np.zeros(len(power[0]))
+        for val in group:
+            tot += power[val]
+        print(Norm(tot))
+
