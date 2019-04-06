1c3f0ba1ee5a76066b99069842a0a4735ca5e762
ewalraven <45516577+ewalraven@users.noreply.github.com>
2019-04-07 00:01:40 +0200

    Add files via upload

--- Dataverwerking.py
+++ Dataverwerking.py
@@ -11,8 +11,9 @@ def read_data(path):
     power = []              # Data of power from excel
     isDayFirst = False      # Used for date parsing
     checkDate = True        # When to stop checking date parsing
-    previousTime = ''       # For checking double time values
-    
+    previousTime = datetime.datetime(1,1,1,1,1)       # For checking double time values
+    timeDiff = 1            # Difference in time between data points
+
     with open(path) as csv_file:
         csv_reader = csv.reader(csv_file,delimiter=';') # Read excel file as .csv
         line_count = 0
@@ -28,46 +29,69 @@ def read_data(path):
                 elif checkDate and row[0][0:3] == '2-1':
                     checkDate = False
                     isDayFirst = True
+
                 # Stores power data
                 temp_line = []
                 for elem in row[2:]:
                     temp_line.append(float(elem))
-                if row[1] == previousTime: # Same time occured one before
+
+                currentTime = parser.parse(row[0]+' '+row[1], dayfirst=isDayFirst)
+                # Determining the time difference between data points
+                if line_count == 2:
+                    timeDiff = int(currentTime.minute-previousTime.minute)
+
+                # Checking whether time is the same or time is skipped (fill in with average)
+                if currentTime == previousTime and line_count != 1: # Same time occured one before
                     power[len(power)-1] = np.divide([power[len(power)-1][i]+temp_line[i] for i in range(len(temp_line))],2)
-                else:
+                elif currentTime != previousTime + datetime.timedelta(minutes=timeDiff) and currentTime != previousTime and line_count != 1: # Time is skipped
+                    previousTime += datetime.timedelta(minutes=timeDiff)
+                    while currentTime != previousTime:
+                        power.append(np.divide([power[len(power)-1][i]+temp_line[i] for i in range(len(temp_line))],2))
+                        time.append(previousTime)
+                        previousTime += datetime.timedelta(minutes=timeDiff)
                     power.append(temp_line)
-                    time.append(parser.parse(row[0]+' '+row[1], dayfirst=isDayFirst))
-                previousTime = row[1]
+                    time.append(currentTime)
+                else:   # Normal time difference
+                    power.append(temp_line)
+                    time.append(currentTime)
+                previousTime = currentTime
+
             if line_count % 10000 == 0:  # Prints progress
                 print(line_count)
             line_count += 1
         print(f'Processed {line_count} lines.')
-    return power, time
+
+    # Delete single value for next year if it's present
+    if time[len(time)-1].day == 1 and time[len(time)-1].month == 1 and time[len(time)-1].hour == 0 and time[len(time)-1].minute == 0:
+        power = power[:-1]
+        time = time[:-1]
+
+    return power,time,timeDiff
 
 #%%
-                  
+
 ''' All data throughout the year '''
 def plot_data(power, time):
-    for i in range(len(power[0])-2):
+    for group in range(len(power[0])):
         # plt.figure()
-        data = [line[i] for line in power]
+        data = [line[group] for line in power]
         plt.plot(time,data)
     plt.show()
 
-#%%                  
-                  
-''' One specific time throughout the year '''
-def plot_specific_time(power, time):
-    data = []
-    timeData = datetime.time(12,0)
+#%%
 
-    for j in range(len(power[0])-2):
+''' One specific time throughout the year '''
+def plot_specific_time(power, time, hour, minute):
+    data = []
+    timeData = datetime.time(hour,minute)
+
+    for group in range(len(power[0])):
         Xdata = []
         Ydata = []
         for i in range(len(power)):
             if time[i].time() == timeData:
                 Xdata.append(time[i])
-                Ydata.append(power[i][j])
+                Ydata.append(power[i][group])
         plt.plot(Xdata,Ydata)
     plt.show()
 
@@ -75,44 +99,42 @@ def plot_specific_time(power, time):
 #%%
 
 # This function is used for manually trimming the data and gives the user the
-# ability to agree on the trimmed data. 
-def manual_trim(power):
-    # time_factor must be 1 if the data is per 15 mins, 3 if it is per 5 mins
-    time_factor = 1
+# ability to agree on the trimmed data.
+def manual_trim(power, timeDiff):
     rows_to_trim = []
     current_row = 0
-    
+
     # For each dataset of one group we check if there are any outliers
     for row in power:
         row_length = len(row)
         maximums = []
         # We check for each day the maximum absolute capacity and store those
         i = 0
-        while 96 * time_factor * (i + 1) < row_length:
+        while 24*60//timeDiff * (i + 1) < row_length:
             maximum = 0
-            for j in range(96 * time_factor):
-                if abs(row[96 * i * time_factor + j]) > maximum:
-                    maximum = abs(row[96 * i * time_factor + j])
+            for j in range(24*60//timeDiff):
+                if abs(row[24*60//timeDiff * i + j]) > maximum:
+                    maximum = abs(row[24*60//timeDiff * i + j])
             maximums.append(maximum)
             i += 1
         plt.plot(list(range(len(maximums))), maximums)
         plt.show()
-        
+
         avg = sum(maximums) / len(maximums)
         print(avg)
-        
+
         suggested_row = np.array([])
         suggested_days = []
         i = 0
-        while 96 * time_factor * (i + 1) < row_length:
+        while 24*60//timeDiff * (i + 1) < row_length:
             if maximums[i] <= 1.5 * avg:
-                suggested_row = np.append(suggested_row, row[96 * time_factor * i : 96 * time_factor * (i + 1)])
+                suggested_row = np.append(suggested_row, row[24*60//timeDiff * i : 24*60//timeDiff * (i + 1)])
                 suggested_days.append(i)
             else:
-                NaNs = np.zeros(96 * time_factor) 
+                NaNs = np.zeros(24*60//timeDiff)
                 suggested_row = np.append(suggested_row, NaNs)
             i += 1
-        
+
         plt.plot(suggested_row)
         plt.show()
         plt.plot(row)
@@ -121,43 +143,41 @@ def manual_trim(power):
         if trim == 'y':
             rows_to_trim.append(current_row)
         current_row += 1
-    
+
     print(rows_to_trim)
 
-# This reads a txt file with the data on which rows to trim and returns the 
+# This reads a txt file with the data on which rows to trim and returns the
 # trimmed data
-def auto_trim(power, rows_to_trim):
-    # time_factor must be 1 if the data is per 15 mins, 3 if it is per 5 mins
-    time_factor = 1
+def auto_trim(power, rows_to_trim, timeDiff):
     suggested_power = []
     current_row = 0
-    
+
     # For each dataset of one group we check if there are any outliers
     for row in power:
-        if current_row in rows_to_trim: 
+        if current_row in rows_to_trim:
             row_length = len(row)
             maximums = []
             # We check for each day the maximum absolute capacity and store those
             i = 0
-            while 96 * time_factor * (i + 1) < row_length:
+            while 24*60//timeDiff * (i + 1) < row_length:
                 maximum = 0
-                for j in range(96 * time_factor):
-                    if abs(row[96 * i * time_factor + j]) > maximum:
-                        maximum = abs(row[96 * i * time_factor + j])
+                for j in range(24*60//timeDiff):
+                    if abs(row[24*60//timeDiff * i + j]) > maximum:
+                        maximum = abs(row[24*60//timeDiff * i + j])
                 maximums.append(maximum)
                 i += 1
-    
+
             avg = sum(maximums) / len(maximums)
-            
+
             suggested_row = np.array([])
             suggested_days = []
             i = 0
-            while 96 * time_factor * (i + 1) < row_length:
+            while 24*60//timeDiff * (i + 1) < row_length:
                 if maximums[i] <= 1.5 * avg:
-                    suggested_row = np.append(suggested_row, row[96 * time_factor * i : 96 * time_factor * (i + 1)])
+                    suggested_row = np.append(suggested_row, row[24*60//timeDiff * i : 24*60//timeDiff * (i + 1)])
                     suggested_days.append(i)
                 else:
-                    NaNs = np.zeros(96 * time_factor) 
+                    NaNs = np.zeros(24*60//timeDiff)
                     suggested_row = np.append(suggested_row, NaNs)
                 i += 1
             suggested_power.append(suggested_row)
@@ -165,14 +185,5 @@ def auto_trim(power, rows_to_trim):
             suggested_power.append(row)
 
         current_row += 1
-    
+
     return suggested_power
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 
 # Enter directory of you files here, so that python knows where to look
-file_dir = r'C:\Users\dion\Documents\Studie\Modellenpracticum'
+file_dir = r'C:\Users\XpsBook\Documents\Radboud Universiteit Nijmegen\Jaar 3\Modellenpracticum'
 sys.path.append(file_dir)
 
 import Dataverwerking as Dv
 import GreedyAlgorithm as GA
-import Karmarkar_karp_v1 as kkk
+import Karmarkar_Karp_v1 as kkk
 import MonteCarloTwist as MCT
 
 #%%
@@ -20,20 +20,20 @@ def Norm(array): #Choose your own norm. Average of the highest n is also possibl
 #%%
 
 
-path1 = r'C:\Users\dion\Documents\Studie\Modellenpracticum\Data\Uilenburg_1_2018_clean.csv'
-path2 = r'C:\Users\dion\Documents\Studie\Modellenpracticum\Data\Uilenburg_2_2018_clean.csv'
+path1 = r'C:\Users\XpsBook\Documents\Radboud Universiteit Nijmegen\Jaar 3\Modellenpracticum\Bestanden Alliander\Power Data\Uilenburg_1_2018_clean.csv'
+path2 = r'C:\Users\XpsBook\Documents\Radboud Universiteit Nijmegen\Jaar 3\Modellenpracticum\Bestanden Alliander\Power Data\Uilenburg_2_2018_clean.csv'
 
 rows_to_trim1 = [0, 1, 2, 3, 4, 5, 8, 9]
 rows_to_trim2 = [0, 2, 3, 4, 7]
 
-power1, time1 = Dv.read_data(path1)
-power2, time2 = Dv.read_data(path2)
+power1, time1, timeDiff1 = Dv.read_data(path1)
+power2, time2, timeDiff2 = Dv.read_data(path2)
 
 power1 = np.transpose(np.array(power1))
 power2 = np.transpose(np.array(power2))
 
-power1 = Dv.auto_trim(power1, rows_to_trim1)
-power2 = Dv.auto_trim(power2, rows_to_trim2)
+power1 = Dv.auto_trim(power1, rows_to_trim1, timeDiff1)
+power2 = Dv.auto_trim(power2, rows_to_trim2, timeDiff2)
 #%%
 power = []
 for row in power1:
@@ -61,4 +61,3 @@ def sol_norms(power, sol, Norm):
         for val in group:
             tot += power[val]
         print(Norm(tot))
-
