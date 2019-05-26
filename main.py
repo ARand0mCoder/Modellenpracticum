525050a4f968f2f9dbe0508e2f59551dc79cd5fe
APBJans <48512914+APBJans@users.noreply.github.com>
2019-05-26 16:20:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,16 @@ def Norm3(array):
         return aamin
 
 #%%
+#Hieronder volgt de lijst met capaciteiten, aantal stekkers, en nummer van de dubbele met weging
+Ubase = 10500
+#BZSV, stekker, dubbel  = [29.4,14.7], [15,5], [] #Bemmel
+#BZSV, stekker, dubbel  = [52,22], [24,24], [] #Hoge Kadijk, in dit bestand kan ik niet netjes lezen of er dubbele kabels zijn...
+#BZSV, stekker, dubbel  = [22,22],[12,12], [[112,114],[124,126]] #Uilenburg
+#BZSV, stekker, dubbel  = [60,60],[17,20], [[107,108,111],[163,164]] #Zevenhuizen
+#BZSV, stekker, dubbel  = [46,23.1],[21,19], [[22,24],[13,15,17],[7,9,11],[44,45],[3,4,5,6,7,8]] #Drachten
+#BZSV, stekker, dubbel  = [], [], [[44,45,54],[43,50,52],[46,55],[48,56,57],[47,58],[32,34],[33,35],[2,6],[9,13]] #Emmeloord
+#BZSV, stekker, dubbel  = [22,44], [12,18], [[103,104],[254,256],[255,259],[260,267],[263,270]], [[302,303],[402,403],[502,503],[602,603]] #Rijksuniversiteit, de derde verzameling heeft 2 kabels per groep nodig, de laatste maar 1 kabel per groep
+#BZSV, stekker, dubbel  = [60.59845,29,31.5],[24,10,17],[[5,6],[3,15],[16,17],[18,19],[85,86],[87,93,94,95],[55,56,57,58,61],[72,75]] #WWG
 
 path1 = r'C:\Users\XpsBook\Documents\Radboud Universiteit Nijmegen\Jaar 3\Modellenpracticum\Power npy\metingen_Uilenburg.npy'
 path2 = r'C:\Users\XpsBook\Documents\Radboud Universiteit Nijmegen\Jaar 3\Modellenpracticum\Bestanden Alliander\Power Data\Uilenburg_2_2018_clean.csv'
