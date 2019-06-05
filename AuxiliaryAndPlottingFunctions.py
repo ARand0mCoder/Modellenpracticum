185cb0a527e08c757b871d331990bd568791c18b
Derfellios <joris.nieuwveld@gmail.com>
2019-06-05 16:20:18 +0200

    Update AuxiliaryAndPlottingFunctions.py

--- AuxiliaryAndPlottingFunctions.py
+++ AuxiliaryAndPlottingFunctions.py
@@ -29,7 +29,7 @@ def DistanceFromOldSol(OldSol, NewSol, PlugsPerField):
     return TotalDistance
    
 
-def DrawDistanceFunction(Solutions, weights):
+def DrawDistanceFunction(Solutions, weights, OldSol):
     NumOfGroups = sum([len(Solutions[1][i]) for i in range(0, len(Solutions[1]))])
     AllDist = [0 for i in range(1, 2*NumOfGroups)]
 	
@@ -37,23 +37,46 @@ def DrawDistanceFunction(Solutions, weights):
     for i in range(0, len(Solutions)):
         for j in range(i + 1, len(Solutions)):
             AllDist[DistanceFromOldSol(Solutions[i], Solutions[j], weights)] += 1
-    
+            
+    FurthestBar = 0
+    for i in range(len(AllDist)):
+        if AllDist[i] >= 1:
+            FurthestBar = i + 5
+    AllDist = AllDist[:FurthestBar-1]
     AllDist = np.array(AllDist)
 	
-	#Plot the solutions.
-    plt.bar([i for i in range(1, 2*NumOfGroups)], AllDist)
+	#Plot the distance of solutions with respect to each other.
+    plt.bar([i for i in range(1, FurthestBar)], AllDist)
+    plt.ylabel("Aantal keer dat dit voorkomt")
+    plt.xlabel("Aantal veranderingen ten opzichte van elkaar")
     plt.show()
     
-    AllDist = [0 for i in range(1, 2*NumOfGroups)]
+    AllDist = [0 for i in range(1, FurthestBar)]
     for j in range(1, len(Solutions)):
         AllDist[DistanceFromOldSol(Solutions[0], Solutions[j], weights)] += 1
     
     AllDist = np.array(AllDist)
 	
-	#Plot the solutions.
-    plt.bar([i for i in range(1, 2*NumOfGroups)], AllDist)
+	#Plot the distance of solutions with respect to the old solution.
+    plt.bar([i for i in range(1, FurthestBar)], AllDist)
+    plt.ylabel("Aantal keer dat dit voorkomt")
+    plt.xlabel("Aantal veranderingen ten opzichte van oude oplossing")
+    plt.show()
+    
+    AllDist = [0 for i in range(1, FurthestBar)]
+    for j in range(1, len(Solutions)):
+        AllDist[DistanceFromOldSol(OldSol, Solutions[j], weights)] += 1
+    
+    AllDist = np.array(AllDist)
+	
+	#Plot the distance of solutions with respect to the best solution found.
+    plt.bar([i for i in range(1, FurthestBar)], AllDist)
+    plt.ylabel("Aantal keer dat dit voorkomt")
+    plt.xlabel("Aantal veranderingen ten opzichte van beste oplossing")
     plt.show()
    
 def PlotAllSolutions(Result):
+    plt.ylabel("Max Norm")
+    plt.xlabel("Beste oplossing")
     plt.plot(Result, "ro")
     plt.show()
